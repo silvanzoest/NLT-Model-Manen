58f84e4f21dbb0e6b5ef8fe22c286823476f310a
kolokkol <jessemaarleveldyt@gmail.com>
2017-09-18 18:43:18 +0200

    Update vector.c

--- src/vector.c
+++ src/vector.c
@@ -1,1 +1,50 @@
+#include <math.h>
+#include <stdlib.h>
 
+#include "vector.h"
+
+Vector *Vector_New(double x, double y) {
+    Vector *vec = malloc(sizeof(Vector));
+    if (vec == NULL) { 
+        return NULL;
+    }
+    vec->x = x;
+    vec->y = y;
+    return vec;
+}
+
+Vector *Vector_FromSize(double angle, double size) {
+    return Vector_New(cos(angle)*size, sin(angle)*size);
+}
+
+double Vector_Size(Vector *v){
+    double x = v->x;
+    double y = v->y;
+    return sqrt(x*x + y*y);
+}
+
+double Vector_Dot(Vector *a, Vector *b) {
+    return a->x * a->b + a->y * b->y;
+}
+
+double Vector_Angle(Vector *a, Vector *b) {
+    double angle = acos(Vector_Dot(a)*Vector_Dot(b) / (Vector_Size(a)*Vector_Size(b));
+    if (angle > pi) {
+        angle = 2*pi - angle;
+    }
+    return angle;
+}
+
+Vector *Vector_Add(Vector *a, Vector *b) {
+    return Vector_New(a->x + b->x, a->y + b->y);
+}
+
+Vector *Vector_Sub(Vector *a, Vector *b) {
+    return Vector_New(a->x - b->x, a->y - b->y);
+}
+
+Vector *Vector_Mul(Vector *v, long long n){
+    x = v->x;
+    y = v->y;
+    return Vector_New(x*n, y*n);
+}
