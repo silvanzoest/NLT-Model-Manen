6bc3e0cbc1ba820d9b386774ce8e4b0522baaacf
kolokkol <jessemaarleveldyt@gmail.com>
2017-09-25 14:46:33 +0200

    Update main.c

--- main.c
+++ main.c
@@ -1,8 +1,16 @@
+#include <math.h>
+
 #include "nlt.h"
+
+const double G = 6.67408e-11;
 
 int main(void) {
     Object_Init();
     Object *mars = Object_New(0, 0, 0, 0, 6.41693e23, , 1);
     Object *phobos = Object_New(0, 9377.2e3, 2137.08977, 0, 10.6e15, 0);
     Object *deimos = Object_New(0, -23460e3, -1351.124444, 0, 1.4762e15, 0);
+    while (1) {
+        double Fg_md = (G* mars->mass* deimos->mass)/  pow(Vector_Size(deimos->location), 2);
+        double Fg_mp = (G* mars->mass* phobos->mass)/  pow(Vector_Size(phobos->location), 2);
+    }
 }
