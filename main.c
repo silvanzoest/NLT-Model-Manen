b1e8dc607a8d6f18c6d5f462ab16f21b438a5d2d
kolokkol <jessemaarleveldyt@gmail.com>
2017-09-25 14:30:24 +0200

    Update main.c

--- main.c
+++ main.c
@@ -1,1 +1,8 @@
 #include "nlt.h"
+
+int main(void) {
+    Object_Init();
+    Object *mars = Object_New(0, 0, 0, 0, 6.41693e23, , 1);
+    Object *phobos = Object_New(0, 9377.2e3, 2137.08977, 0, 10.6e15, 0);
+    Object *deimos = Object_New(0, -23460e3, -1351.124444, 0, 1.4762e15, 0);
+}
